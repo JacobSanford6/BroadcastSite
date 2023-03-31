922bce6497e313106de46c0fde5fb9ddfbd2b1b7
Jake Sanford <jacob.r.sanford@gmail.com>
2023-03-31 14:50:52 -0500

    modified server to be able to accept both json and formdata post requests

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
 from flask import Flask, render_template, url_for, request
+from waitress import serve
 import json
+import time
 
 app = Flask(__name__)
 
 broadcast = "None"
 action = 0
-img = None
-
+broadcastImageHtml = "<p></p>"
 
 def handlePost():
     global broadcast
-    data = json.loads(request.data)
-    print(data['broadcast'])
+    global broadcastImageHtml
 
-    
-    if data['broadcast']:
-        broadcast = data['broadcast']
+    if request.content_type=="application/json":
+        data = json.loads(request.data)
+        print(data['broadcast'])
+
         
-        return json.dumps({'success':True})
-    else:
+        if data['broadcast']:
+            broadcast = data['broadcast']
+            
+            return json.dumps({'success':True})
+        else:
+            return "{success:false}"
+    elif request.content_type.split(";")[0] == "multipart/form-data":
+        data = request.form
+        
+
+        print(data.keys())
+        
+        if data["broadcastImage"]:
+            print(type(data["broadcastImage"]))
+            print("Printing image binary data in 2 seconds")
+            time.sleep(2)
+            print(data["broadcastImage"])
+        
         return "{success:false}"
 
 
@@ -27,10 +44,11 @@ def handlePost():
 def index():
     if request.method == 'GET':
         print(broadcast)
-        return render_template("index.html", broadcast=broadcast, action=action, img=img)
+        return render_template("index.html", broadcast=broadcast, action=action, broadcastImage=broadcastImageHtml)
     elif request.method == 'POST':
         return handlePost()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    print("Server Launched on Port 5000")
+    serve(app, host="0.0.0.0", port="5000")
