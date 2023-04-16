582fe3ad09911704743eec9f627b8fadada85acf
Jake Sanford <jacob.r.sanford@gmail.com>
2023-04-16 13:28:13 -0500

    Added broadcast image, and cleaned up code

--- app.py
+++ app.py
@@ -2,52 +2,46 @@ from flask import Flask, render_template, url_for, request
 from waitress import serve
 import json
 import time
+from PIL import Image
+import random
+import os
 
-app = Flask(__name__)
-
+app = app = Flask(__name__)
 broadcast = "None"
-action = 0
-broadcastImageHtml = "<p></p>"
 
 def handlePost():
     global broadcast
-    global broadcastImageHtml
 
     if request.content_type=="application/json":
         data = json.loads(request.data)
-        print(data['broadcast'])
-
-        
-        if data['broadcast']:
-            broadcast = data['broadcast']
-            
-            return json.dumps({'success':True})
+        if data["broadcast"]:
+            broadcast = data["broadcast"]
+            return "{success:true}"
         else:
             return "{success:false}"
-    elif request.content_type.split(";")[0] == "multipart/form-data":
-        data = request.form
-        
+    elif len(request.files) > 0:
+        localtime = time.localtime()
+        timeNum = int( str( localtime.tm_hour+localtime.tm_min ) + str(localtime.tm_min) )
 
-        print(data.keys())
-        
-        if data["broadcastImage"]:
-            print(type(data["broadcastImage"]))
-            print("Printing image binary data in 2 seconds")
-            time.sleep(2)
-            print(data["broadcastImage"])
-        
-        return "{success:false}"
+        if "secret-key" in request.form:
+            if "broadcastImage" in request.files and request.form["secret-key"] == str(timeNum):
+                print("saved new image")
+                bimg = request.files["broadcastImage"]
+                bimg.save("C:\\Users\\Jake\\chainReactionLinkSite\\static\\broadImage.jpg")
+                return "{success:true}"
+            else:
+                print(request.form["secret-key"])
+                print(str(timeNum))
+                return "{success:false}"
+    else:
+        return"{success:false}"
 
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'GET':
-        print(broadcast)
-        return render_template("index.html", broadcast=broadcast, action=action, broadcastImage=broadcastImageHtml)
-    elif request.method == 'POST':
+    if request.method == "GET":
+        return render_template("index.html", broadcast=broadcast)
+    elif request.method == "POST":
         return handlePost()
-
 
 if __name__ == "__main__":
     print("Server Launched on Port 5000")
