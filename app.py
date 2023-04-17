1fc1d1c0c9000cf35603367a504143a615b1a58a
Jake Sanford <jacob.r.sanford@gmail.com>
2023-04-16 21:09:29 -0500

    added xlsx to json to serve mobile app schedule data

--- app.py
+++ app.py
@@ -5,9 +5,35 @@ import time
 from PIL import Image
 import random
 import os
+import openpyxl
+from pathlib import Path
 
 app = app = Flask(__name__)
 broadcast = "None"
+
+def readXlsx():
+    wb_obj = openpyxl.load_workbook( "C:\\Users\\Jake\\chainReactionLinkSite\\static\\schedule.xlsx" )
+    latestSheetName = wb_obj.sheetnames[ len(wb_obj.sheetnames)-1]
+    latestSheet = wb_obj[latestSheetName]
+    latestSheet = latestSheet
+
+    eTable = []
+    count = 0
+
+    for row in latestSheet:
+        if count == 0:
+            count = 1
+            continue
+        nEmployee = []
+        for column in row:
+            if column.value != None:
+                if column.value.strip() == "" or column.value.strip() == " ":
+                    nEmployee.append("X/X")
+                else:
+                    nEmployee.append(column.value.strip())
+        if len(nEmployee) > 0:
+            eTable.append(nEmployee)
+    return eTable
 
 def handlePost():
     global broadcast
@@ -25,13 +51,10 @@ def handlePost():
 
         if "secret-key" in request.form:
             if "broadcastImage" in request.files and request.form["secret-key"] == str(timeNum):
-                print("saved new image")
                 bimg = request.files["broadcastImage"]
                 bimg.save("C:\\Users\\Jake\\chainReactionLinkSite\\static\\broadImage.jpg")
                 return "{success:true}"
             else:
-                print(request.form["secret-key"])
-                print(str(timeNum))
                 return "{success:false}"
     else:
         return"{success:false}"
@@ -42,6 +65,28 @@ def index():
         return render_template("index.html", broadcast=broadcast)
     elif request.method == "POST":
         return handlePost()
+
+@app.route("/xlsxFile", methods=["GET", "SET"])
+def xlsxFile():
+    if request.method == "GET":
+        nDict = {}
+
+        nArr =readXlsx()
+        
+        nDict["days"] = nArr[0]
+        
+        for row in nArr[1::]:
+            name = row[0].split(" ")[0]
+            if name.strip() == "":
+                name = row[0].split(" ")[1]
+            if name.strip() == "":
+                name = "err"
+            if name != "err":
+                nDict[name] = row[1::]
+
+        return json.dumps(nDict)
+    else:
+        return "{test:'No'}"
 
 if __name__ == "__main__":
     print("Server Launched on Port 5000")
